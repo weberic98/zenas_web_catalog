28eb4132cf6a689216f1a1de9aa17eaa1a8b8454
weberic98 <156134452+weberic98@users.noreply.github.com>
2024-03-07 13:31:53 -0600

    Update zenas.py

--- zenas.py
+++ zenas.py
@@ -12,7 +12,7 @@ my_cur = my_cnx.cursor()
 
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from catalog_for_website")
-my_catalog = my_cur.fetchall()
+my_catalog = my_cur.fetchone()
 
 # put the dafta into a dataframe 
 df = pandas.DataFrame(my_catalog)
