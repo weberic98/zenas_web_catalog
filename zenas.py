aade8b16d799137ae8a7a317f81a44fd84b960f5
weberic98 <156134452+weberic98@users.noreply.github.com>
2024-03-07 13:11:01 -0600

    Update zenas.py

--- zenas.py
+++ zenas.py
@@ -21,6 +21,6 @@ streamlit.dataframe(my_data_rows)
 #my_clothing_list = my_data_rows.set_index('COLOR_OR_STYLE')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-clothing_selected = streamlit.select("Pick some clothing:", list(my_data_rows))
+clothing_selected = streamlit.selectbox("Pick some clothing:", list(my_data_rows))
 #clothing_to_show = my_clothing_list.loc[clothing_selected]
 
