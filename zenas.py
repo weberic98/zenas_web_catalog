5f42262756fa67172c0fa0dc1d6ef6f62652dd99
weberic98 <156134452+weberic98@users.noreply.github.com>
2024-03-07 12:50:32 -0600

    Update zenas.py

--- zenas.py
+++ zenas.py
@@ -6,15 +6,19 @@ from urllib.error import URLError
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
-streamlit.header("Pick a sweatsuit color or style")
+#streamlit.header("Pick a sweatsuit color or style")
 #Snowflake-related functions
 def get_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from catalog_for_website")
         return my_cur.fetchall()
 
-clothing_selected = streamlit.multiselect("Pick a sweatsuit color or style:", list(get_load_list.index))
-clothing_to_show = my_clothing_list.loc[clothing_selected]
+# Add a button to load the fruit
+if streamlit.button('Pick a sweatsuit color or style'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_load_list()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
 
 
 
